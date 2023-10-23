267d8f5077a447b01f6da905c2bc6216e8c9009a
Haruray <syafiqfaray2@gmail.com>
2023-10-23 13:53:43 +0700

    add kwargs

--- src/indobenchmark/tokenization_indonlg.py
+++ src/indobenchmark/tokenization_indonlg.py
@@ -359,8 +359,8 @@ class IndoNLGTokenizer(PreTrainedTokenizer):
         self.sp_model = spm.SentencePieceProcessor(**self.sp_model_kwargs)
         self.sp_model.Load(self.vocab_file)
 
-    def decode(self, inputs, skip_special_tokens=False, clean_up_tokenization_spaces = True):     
-        outputs = super().decode(inputs, skip_special_tokens=skip_special_tokens, clean_up_tokenization_spaces=clean_up_tokenization_spaces)
+    def decode(self, inputs, skip_special_tokens=False, clean_up_tokenization_spaces = True, **kwargs):     
+        outputs = super().decode(inputs, skip_special_tokens=skip_special_tokens, clean_up_tokenization_spaces=clean_up_tokenization_spaces, **kwargs)
         return outputs.replace(' ','').replace('▁', ' ')
     
     def _pad_decoder(
